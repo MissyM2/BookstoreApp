d43e60450f988545e280599134359d0d2e2224c0
maloney_f <missymaloney1@gmail.com>
2022-11-02 15:56:41 -0400

    Added EFCore and Scaffolded Database

--- BookStoreApp.API/Program.cs
+++ BookStoreApp.API/Program.cs
@@ -1,8 +1,15 @@
+using BookStoreApp.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connString = builder.Configuration.GetConnectionString("BookStoreAppDbConnection");
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer());
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
