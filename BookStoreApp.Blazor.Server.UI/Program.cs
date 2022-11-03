ec29a6ef9e1e5ddfb297e4640ffa07208e4cd27b
maloney_f <missymaloney1@gmail.com>
2022-11-03 17:55:45 -0400

    Cleanup and fix on ConfigMapper

---
+++ BookStoreApp.Blazor.Server.UI/Program.cs
@@ -0,0 +1,31 @@
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient<IClient, Client>(cl => cl.BaseAddress= new Uri("https://localhost:7110"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
