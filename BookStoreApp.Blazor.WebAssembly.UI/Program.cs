1c8b1a6bc7a44c66ff7adbcedd0930be1334abd8
Trevoir Williams <trevoir.williams@gmail.com>
2022-11-12 10:46:05 -0500

    Update Program.cs

--- BookStoreApp.Blazor.WebAssembly.UI/Program.cs
+++ BookStoreApp.Blazor.WebAssembly.UI/Program.cs
@@ -13,7 +13,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7024") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7110") });
 
 builder.Services.AddBlazoredLocalStorage();
 
